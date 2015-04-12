e5460fdbc5811299016e454db7ad02a890baf44a
manazevedof <mario.azevedo@gmail.com>
2015-04-12 20:15:58 -0300

    other

---
+++ plot4.R
@@ -0,0 +1,4 @@
+#data <- read.table("household_power_consumption.txt",sep=';',header=TRUE,na.strings = "?")
+#data$timestamp <- strptime(paste(data$Date,data$Time),"%d/%m/%Y %H:%M:%S")
+#data <- subset(data, timestamp >= "2007-02-01 00:00:00" & timestamp <= "2007-02-02 23:59:59")
+
